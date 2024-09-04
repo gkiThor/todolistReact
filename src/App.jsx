8d6a64029241c18fed09cdea9f876bb0790c801a
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-09-04 04:51:46 +0200

    validation et ajout de taches

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,27 @@ function App() {
     {id:nanoid(6), content: 'item 3'}
   ])
 
+  const [todo, setTodo] = useState("")
+  const [showValidation, setShowValidation] = useState(false)
+
   console.log(todoList);
 
+  /* Suppression d'une tache */
   function deleteTodo(id){
-    setTodoList(todoList.filter(todo => todo.id !== id))
+    setTodoList(todoList.filter(todo => todo.id !== id));
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    if(todo === ""){
+      setShowValidation(true);
+      return
+    }
+
+    setTodoList([...todoList, {id: nanoid(), todo}]);/* Nouveau tableau */
+    setTodo("");
+    setShowValidation(false);
   }
 
   return (
@@ -25,9 +42,16 @@ function App() {
         <h1 className="text-3xl text-slate-950 text-center">To-do liste</h1>
 
         {/*  Formulaire */}
-        <form className="mb-10">
+        <form onSubmit={handleSubmit} className="mb-10">
           <label htmlFor="todo-item" className="text-slate-950">Ajouter une tâche ...</label>
-          <input type="text" className="mt-1 block w-full rounded"/>{/* Le input text prend toute la largeur */}
+          <input value={todo} onChange={e => setTodo(e.target.value)} type="text" className="mt-1 block w-full rounded"/>{/* Le input text prend toute la largeur */}
+         
+          {showValidation && (
+            <p className="text-red-500">
+              Ajoutez du contenu à la liste de tâche
+            </p>
+          )}
+
           <button className="mt-4 py-2 bg-slate-600 rounded min-w-[120px] text-slate-50">Ajouter</button>
         </form>
 
@@ -38,7 +62,8 @@ function App() {
           )}
 
 
-          {todoList.map(item => (
+          {todoList.length > 0 && 
+          todoList.map(item => (
              <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo}/>
           ))}
         </ul>
