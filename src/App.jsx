29679f4ea7fa193f330bf11783aad29d33b6e852
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-09-04 02:12:14 +0200

    mise en place du projet todoList: vite + tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="bg-red-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus eligendi, animi quibusdam architecto eum ut libero! Nostrum quisquam perspiciatis soluta quidem aliquam vero hic labore, fugiat placeat, officiis cumque et.</h1>
+    </>
+  )
+}
+
+export default App
