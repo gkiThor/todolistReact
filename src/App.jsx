dccef414e8507bccf4456bdd2f109b35caa2324c
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-09-04 02:52:58 +0200

    ajout du formulaire

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@
 function App() {
   
   return (
+    /* Structure de base avec fond gris clair*/
     <div className="h-screen bg-slate-300">
       
+      {/*  Structure principal contenant le titre centrer */}
       <div className="max-w-4xl mx-auto pt-20 px-6 ">
         <h1 className="text-3xl text-slate-950 text-center">To-do liste</h1>
+
+        {/*  Formulaire */}
+        <form className="mb-10">
+          <label htmlFor="todo-item" className="text-slate-950">Ajouter une tâche ...</label>
+          <input type="text" className="mt-1 block w-full rounded"/>{/* Le input text prend toute la largeur */}
+          <button className="mt-4 py-2 bg-slate-600 rounded min-w-[120px] text-slate-50">Ajouter</button>
+        </form>
+        
       </div>
 
     </div>
